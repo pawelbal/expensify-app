6f8f990fecacaeb786149c5d58c2f264108ab45a
pawelbal <pawelbal@op.pl>
2021-04-23 14:29:35 +0200

    Setup DB and test database env

---
+++ src/firebase/firebase.js
@@ -0,0 +1,151 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASURMENT_ID,
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
+
+export { firebase, database as default };
+
+// // child_removed
+// database.ref('expenses').on('child_removed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+// // child_changed
+// database.ref('expenses').on('child_changed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+// // child_added
+// database.ref('expenses').on('child_added', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+// // database
+// //   .ref('expenses')
+// //   .once('value')
+// //   .then((snapshot) => {
+// //     const expenses = [];
+
+// //     snapshot.forEach((childSnapshot) => {
+// //       expenses.push({
+// //         id: childSnapshot.key,
+// //         ...childSnapshot.val(),
+// //       });
+// //     });
+// //     console.log(expenses);
+// //   });
+
+// // database.ref('expenses').on('value', (snapshot) => {
+// //   const expenses = [];
+
+// //   snapshot.forEach((childSnapshot) => {
+// //     expenses.push({
+// //       id: childSnapshot.key,
+// //       ...childSnapshot.val(),
+// //     });
+// //   });
+// //   console.log(expenses);
+// // });
+
+// // database.ref('expenses').push({
+// //   description: 'Rent 2',
+// //   note: 'This mounth rent',
+// //   amount: 250,
+// //   createdAt: 10000,
+// // });
+
+// // database.ref('notes/-MYxtSkewKx8VyiCXSMb').remove();
+
+// // database.ref('notes').push({
+// //   title: 'Course Topics',
+// //   body: 'Some crazy shit',
+// // });
+
+// // database.ref().on('value', (snapshot) => {
+// //   console.log(
+// //     `${snapshot.val().name} is a ${snapshot.val().job.title} at ${
+// //       snapshot.val().job.company
+// //     }`
+// //   );
+// // });
+
+// // const onValueChange = database.ref().on('value', (snapshot) => {
+// //   console.log(snapshot.val());
+// // }, (e) => {
+// //   console.log('Error with data fetching', e)
+// // });
+
+// // setTimeout(() => {
+// //   database.ref('age').set(29);
+// // }, 3500);
+
+// // setTimeout(() => {
+// //   database.ref().off(onValueChange);
+// // }, 7000);
+
+// // setTimeout(() => {
+// //   database.ref('age').set(30);
+// // }, 10500);
+
+// // database
+// //   .ref('location/city')
+// //   .once('value')
+// //   .then((snapshot) => {
+// //     const val = snapshot.val();
+// //     console.log(val);
+// //   })
+// //   .catch((e) => {
+// //     console.log('Error fatching data', e);
+// //   });
+
+// // database
+// //   .ref()
+// //   .set({
+// //     name: 'Pawel Be',
+// //     age: 28,
+// //     job: {
+// //       title: 'Developer',
+// //       company: 'Company',
+// //     },
+// //     stressLevel: 6,
+// //     location: {
+// //       city: 'BSK',
+// //       country: 'Poland',
+// //     },
+// //   })
+// //   .then(() => {
+// //     console.log('Data is saved!');
+// //   })
+// //   .catch((e) => {
+// //     console.log('This failed.', e);
+// //   });
+
+// // database.ref().update({
+// //   stressLevel: 9,
+// //   'job/company': 'Amazon',
+// //   'location/city': 'Warsaw',
+// // });
+
+// // database
+// //   .ref('isSingle')
+// //   .remove()
+// //   .then(() => {
+// //     console.log('Data removed.');
+// //   })
+// //   .catch((e) => {
+// //     console.log('This failed: ', e);
+// //   });
--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,4 +1,15 @@
-import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import {
+  startAddExpense,
+  addExpense,
+  editExpense,
+  removeExpense,
+} from '../../actions/expenses';
+import expenses from '../fixtures/expenses';
+import database from '../../firebase/firebase';
+
+const createMockStore = configureMockStore([thunk]);
 
 test('should setup remove expense action object', () => {
   const result = removeExpense({ id: '123abc' });
@@ -22,30 +33,80 @@ test('should setup edit expense action object', () => {
 });
 
 test('should set up add expense action object with provided values', () => {
+  const action = addExpense(expenses[2]);
+  expect(action).toEqual({
+    type: 'ADD_EXPENSE',
+    expense: expenses[2],
+  });
+});
+
+test('should add expense to DB and store', (done) => {
+  const store = createMockStore({});
   const expenseData = {
-    description: 'Rent',
-    note: 'This is last month rent',
-    amount: 1000,
+    description: 'Laptop',
+    amount: '400000',
+    note: 'New laptop for work',
     createdAt: 1000,
   };
 
-  const action = addExpense(expenseData);
-  expect(action).toEqual({
-    type: 'ADD_EXPENSE',
-    expense: { ...expenseData, id: expect.any(String) },
-  });
+  store
+    .dispatch(startAddExpense(expenseData))
+    .then(() => {
+      const actions = store.getActions();
+      expect(actions[0]).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+          id: expect.any(String),
+          ...expenseData,
+        },
+      });
+      return database.ref(`expenses/${actions[0].expense.id}`).once('value');
+    })
+    .then((snapshot) => {
+      expect(snapshot.val()).toEqual(expenseData);
+      done();
+    });
 });
 
-test('should set up add expense action object with default values', () => {
-  const action = addExpense();
-  expect(action).toEqual({
-    type: 'ADD_EXPENSE',
-    expense: {
-      id: expect.any(String),
-      description: '',
-      note: '',
-      amount: 0,
-      createdAt: 0,
-    },
-  });
+test('should add expense with defaults to DB and store', (done) => {
+  const store = createMockStore({});
+  store
+    .dispatch(startAddExpense({}))
+    .then(() => {
+      const actions = store.getActions();
+      expect(actions[0]).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+          id: expect.any(String),
+          description: '',
+          note: '',
+          amount: 0,
+          createdAt: 0,
+        },
+      });
+      return database.ref(`expenses/${actions[0].expense.id}`).once('value');
+    })
+    .then((snapshot) => {
+      expect(snapshot.val()).toEqual({
+        description: '',
+        note: '',
+        amount: 0,
+        createdAt: 0,
+      });
+      done();
+    });
 });
+
+// test('should set up add expense action object with default values', () => {
+//   const action = addExpense();
+//   expect(action).toEqual({
+//     type: 'ADD_EXPENSE',
+//     expense: {
+//       id: expect.any(String),
+//       description: '',
+//       note: '',
+//       amount: 0,
+//       createdAt: 0,
+//     },
+//   });
+// });
