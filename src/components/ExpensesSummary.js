d6b776aa9303ecc3058b334f1a44320cb0e8e5be
pawelbal <pawelbal@op.pl>
2021-04-29 11:23:53 +0200

    logout button, refactoring and styling ExpensesSummary

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
 import numeral from 'numeral';
@@ -7,11 +8,18 @@ import numeral from 'numeral';
 export const ExpensesSummary = ({ expensesCount, expensesTotal }) => {
   const expenseWord = expensesCount === 1 ? 'expense' : 'expenses';
   return (
-    <div>
-      <h1>
-        Viewing {expensesCount} {expenseWord} totalling:{' '}
-        {numeral(expensesTotal / 100).format('$0,0.00')}
-      </h1>
+    <div className="page-header">
+      <div className="content-container">
+        <h1 className="page-header__title">
+          Viewing <span>{expensesCount}</span> {expenseWord} totalling:{' '}
+          <span>{numeral(expensesTotal / 100).format('$0,0.00')}</span>
+        </h1>
+        <div className="page-header__actions">
+          <Link className="button" to="/create">
+            Add expense
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,9 @@ export const Header = ({ startLogout }) => (
         <Link className="header__title" to="/dashboard">
           <h1>Expensify</h1>
         </Link>
-        <button onClick={startLogout}>Logout</button>
+        <button onClick={startLogout} className="button button__link">
+          Logout
+        </button>
       </div>
     </div>
   </header>
