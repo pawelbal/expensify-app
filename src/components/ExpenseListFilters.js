6f19c31f6c07e9ee86462bb12c3dcd8e4ad44401
pawelbal <pawelbal@op.pl>
2021-04-29 11:39:16 +0200

    styling filters

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -30,28 +30,39 @@ export class ExpenseListFilters extends React.Component {
   };
   render() {
     return (
-      <div>
-        <input
-          type="text"
-          value={this.props.filters.text}
-          onChange={this.onTextChange}
-        ></input>
-        <select value={this.props.filters.sortBy} onChange={this.onsSortChange}>
-          <option value="date">Date</option>
-          <option value="amount">Amount</option>
-        </select>
-        <DateRangePicker
-          startDate={this.props.filters.startDate}
-          endDate={this.props.filters.endDate}
-          onDatesChange={this.onDatesChange}
-          focusedInput={this.state.calendarFocused}
-          onFocusChange={this.onFocusChange}
-          showClearDates={true}
-          numberOfMonths={1}
-          isOutsideRange={() => false}
-          startDateId={'dwjkhqkehwqjkeq'}
-          endDateId={'cxzvcxbzbczxbz'}
-        />
+      <div className="content-container">
+        <div className="input-group">
+          <div className="input-group__item">
+            <input
+              type="text"
+              value={this.props.filters.text}
+              onChange={this.onTextChange}
+            ></input>
+          </div>
+          <div className="input-group__item">
+            <select
+              value={this.props.filters.sortBy}
+              onChange={this.onsSortChange}
+            >
+              <option value="date">Date</option>
+              <option value="amount">Amount</option>
+            </select>
+          </div>
+          <div className="input-group__item">
+            <DateRangePicker
+              startDate={this.props.filters.startDate}
+              endDate={this.props.filters.endDate}
+              onDatesChange={this.onDatesChange}
+              focusedInput={this.state.calendarFocused}
+              onFocusChange={this.onFocusChange}
+              showClearDates={true}
+              numberOfMonths={1}
+              isOutsideRange={() => false}
+              startDateId={'dwjkhqkehwqjkeq'}
+              endDateId={'cxzvcxbzbczxbz'}
+            />
+          </div>
+        </div>
       </div>
     );
   }
