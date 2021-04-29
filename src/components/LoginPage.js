0dec91b2ebbbc6d1ed61cfcfadadc43495863c55
pawelbal <pawelbal@op.pl>
2021-04-29 11:05:45 +0200

    login button, styling header

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -6,9 +6,11 @@ export const LoginPage = ({ startLogin }) => {
   return (
     <div className="box-layout">
       <div className="box-layout__box">
-        <h1 className="box-layout__title">Expensify App</h1>
+        <h1 className="box-layout__title">Expensify</h1>
         <p>It's time to get your expenses under control.</p>
-        <button onClick={startLogin}>Login</button>
+        <button onClick={startLogin} className="button">
+          Login with Google
+        </button>
       </div>
     </div>
   );
